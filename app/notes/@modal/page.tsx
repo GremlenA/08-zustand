d85e701d292fe31de47ef1009afdd8b99ad08b12
GremlenA <pedenkoartem5@gmail.com>
2025-12-11 22:24:21 +0200

    16

---
+++ app/notes/@modal/page.tsx
@@ -0,0 +1,7 @@
+"use client"; 
+import Modal from "@/components/Modal/Modal"; 
+import NotePreview from "@/components/NotePreview/NotePreview"; 
+export default function ModalNotePage({ params }: { params: { id: string } })
+ { const handleClose = () => window.history.back(); 
+    return 
+ }

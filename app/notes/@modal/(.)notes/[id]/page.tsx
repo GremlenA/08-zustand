dff99c9bcc29ac5334572ec5c77569bac49ae9dc
GremlenA <pedenkoartem5@gmail.com>
2025-12-12 10:24:51 +0200

    fix id problem

--- app/notes/@modal/(.)notes/[id]/page.tsx
+++ app/notes/@modal/(.)notes/[id]/page.tsx
@@ -3,12 +3,16 @@
 import Modal from "@/components/Modal/Modal";
 import NotePreview from "@/components/NotePreview/NotePreview";
 
-export default function ModalNotePage({ params }: { params: { id: string } }) {
+export default function ModalNotePage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const handleClose = () => window.history.back();
 
   return (
     <Modal onClose={handleClose}>
-      <NotePreview id={params.id} />
+      <NotePreview noteId={params.id} />
     </Modal>
   );
 }
